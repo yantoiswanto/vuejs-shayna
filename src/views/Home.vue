bc0281b4b7fa27bc95c591b01f9b0dd33bf28c25
yanto <yantoiswanto18@gmail.com>
2021-10-07 15:39:12 +0700

    add module vuejs

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero/>
+    <Woman/>
+    <Instagram />
+    <LogoPartner/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Hero from '@/components/Hero.vue';
+import Woman from '@/components/WomanProduct.vue';
+import Instagram from '@/components/Instagram.vue'
+import LogoPartner from '@/components/LogoPartner.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Woman,
+    Instagram,
+    LogoPartner,
+    Footer
+  }
+}
+</script>
+
